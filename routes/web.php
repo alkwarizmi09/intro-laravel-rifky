a98d51999a6f9f59b8672c367428afc8f8c766c3
alkwarizmi09 <prifki859@gmail.com>
2024-08-22 20:53:18 +0700

    tugas ke-13 dan ke-14

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AuthController;
+use Spatie\FlareClient\View;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
 Route::get('/welcome', [AuthController::class, 'showWelcomePage'])->name('welcome');
+Route::get('/table', function () {
+    return view('tabel', [
+        'title' => 'tabel'
+    ]);
+});
+Route::get('/tabeldata', function () {
+    return view('tabel_data', [
+        'title' => 'tabel data'
+    ]);
+});
